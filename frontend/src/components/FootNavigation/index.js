9c5ca2c57de9acfa05c4d26c103d3f724d22654c
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-14 12:48:13 -0500

    Issue with Splash styling

--- frontend/src/components/FootNavigation/index.js
+++ frontend/src/components/FootNavigation/index.js
@@ -1,8 +1,11 @@
+import React from "react";
 import "./FootNav.css";
 
 function FootNav() {
     return (
-        <h3>This should be in the footer</h3>
+        <React.Fragment className="footer">
+            <h3>This should be in the footer</h3>
+        </React.Fragment>
     );
 }
 
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./Splash.css";
 
 function Splash() {
 
     return (
-        <>
+        <React.Fragment className="splash-body">
             <h1>This is a heading</h1>
             <h3>Some random filler text</h3>
-        </>
+        </React.Fragment>
     );
 }
 
