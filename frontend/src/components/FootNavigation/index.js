33cdbb20d9104018b807a63d5c9e0316a414f97d
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-14 11:58:58 -0500

    Added Splash component and FootNav component

---
+++ frontend/src/components/FootNavigation/index.js
@@ -0,0 +1,9 @@
+import "./FootNav.css";
+
+function FootNav() {
+    return (
+        <h3>This should be in the footer</h3>
+    );
+}
+
+export default FootNav;
---
+++ frontend/src/components/Splash/index.js
@@ -0,0 +1,13 @@
+import "./Splash.css";
+
+function Splash() {
+
+    return (
+        <>
+            <h1>This is a heading</h1>
+            <h3>Some random filler text</h3>
+        </>
+    );
+}
+
+ export default Splash;
