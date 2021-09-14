de0f7bb37a67615a8212fed53c2df572f5594b62
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-14 15:33:19 -0500

    Changed FootNav div name to footer-nav

--- frontend/src/components/FootNavigation/index.js
+++ frontend/src/components/FootNavigation/index.js
@@ -3,7 +3,7 @@ import "./FootNav.css";
 
 function FootNav() {
     return (
-        <div className="footer">
+        <div className="footer-nav">
             <h3>This should be in the footer</h3>
         </div>
     );
