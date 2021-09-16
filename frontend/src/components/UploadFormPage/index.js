4cae3d23b9ebdd72ce204417fc86758617cf2156
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-15 21:31:22 -0500

    Rendered Albums on front end successfully

--- frontend/src/components/UploadFormPage/index.js
+++ frontend/src/components/UploadFormPage/index.js
@@ -1,4 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { getAllAlbums } from "../../store/albums";
+
 function UploadFormPage() {
+    const dispatch = useDispatch();
+    let albumObj = useSelector(state => state.album);
+    let allAlbums = albumObj.albums;
+
+    useEffect(() => {
+        dispatch(getAllAlbums());
+    }, [dispatch]);
+
     return (
         <h1>This is the upload form!</h1>
     )
