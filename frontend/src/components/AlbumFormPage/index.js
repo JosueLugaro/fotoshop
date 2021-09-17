ca8c61d7bec21f79acf73a174f8a39a15a0faceb
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-17 14:41:19 -0500

    Created array of selected photos in AlbumFormPage

--- frontend/src/components/AlbumFormPage/index.js
+++ frontend/src/components/AlbumFormPage/index.js
@@ -1,23 +1,48 @@
 import { getAllPhotos } from '../../store/photos';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./AlbumFormPage.css";
 
 function AlbumFormPage() {
     const dispatch = useDispatch();
     let pics = useSelector(state => state.photo.photos);
+    let user = useSelector(state => state.session.user);
     let possiblePhotos = pics.filter((photo) => (
-        photo.albumId === null
-    ))
+        photo.albumId === null && photo.userId === user.id
+    ));
+    let [selectedPhotos, setSelectedPhotos] = useState([]);
 
     useEffect(() => (
         dispatch(getAllPhotos())
     ), [dispatch]);
 
+    // useEffect(() => {
+
+
+    //     setSelectedPhotos(newArray);
+    // }, [possiblePhotos]);
+
+    const handleChange = (photoId) => {
+        let newArray = selectedPhotos.slice();
+
+        if(newArray.includes(photoId)) {
+            let index = newArray.indexOf(photoId)
+            newArray.splice(index, 1);
+        } else {
+            newArray.push(photoId);
+        }
+
+        setSelectedPhotos(newArray);
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return (
         <div>
             <h1>This is the album form page</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Album Name:
                     <input type="text"/>
@@ -26,13 +51,15 @@ function AlbumFormPage() {
                     Photos:
                 </label>
                     <div className="option-wrapper">
-                        {possiblePhotos.map((photo) => (
-                            <div className="option-image-container">
-                                <img src={photo.imageUrl} alt="stuff" className="option-image"/>
-                                <input type="checkbox" value={photo.id} className="option-input"/>
+                        {possiblePhotos.map((photo, index) => (
+                            <div className="option-image-container" key={index}>
+                                <img src={photo.imageUrl} alt="stuff" className="option-image" key={index}/>
+                                <input type="checkbox" value={photo.id} key={photo.id} className="option-input" checked={selectedPhotos.includes(photo.id)} onChange={(e) => handleChange(photo.id)}/>
                             </div>
                         ))}
                     </div>
+
+                <button type="submit">Create Album!</button>
             </form>
         </div>
     )
