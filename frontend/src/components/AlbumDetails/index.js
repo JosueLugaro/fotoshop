2e1844844d6704dababd467d9358c5b07bf561ef
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-18 14:57:33 -0500

    Can now read albums with there appropriate photos

--- frontend/src/components/AlbumDetails/index.js
+++ frontend/src/components/AlbumDetails/index.js
@@ -1,6 +1,35 @@
+import { useParams, useHistory, NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { getOneAlbum } from "../../store/albums";
+import { getAllPhotos } from '../../store/photos';
+import "./AlbumDetails.css";
+
 function AlbumDetails() {
+    const params = useParams();
+    const dispatch = useDispatch();
+    let history = useHistory();
+
+    let album = useSelector(state => state.album.currentAlbum);
+    let photos = useSelector(state => state.photo.photos);
+
+    let albumPhotos = photos.filter((photo) => (
+        photo.albumId === album.id
+    ))
+
+    useEffect(() => {
+        dispatch(getOneAlbum(params.albumId));
+        dispatch(getAllPhotos());
+    }, [dispatch, params.albumId]);
+
+
     return (
-        <h1>This is the albums details page!</h1>
+        <div className="album-details-container">
+            <h1>This is the albums details page for the {album.title} album</h1>
+            {albumPhotos && albumPhotos.map((photo) => (
+                <img src={photo.imageUrl} alt={photo.title} className="album-photo"/>
+            ))}
+        </div>
     )
 }
 
