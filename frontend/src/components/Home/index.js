585b34da7e12fecace1b8b7baaa6c8c85d895db8
Josue Lugaro <josuelugaro15@gmail.com>
2021-09-19 22:39:29 -0500

    Styled signed out splash home page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -9,19 +9,26 @@ function Home() {
     let page;
     if(sessionUser) {
         page = (
-            <div className="signed-in">
-                <h1>We're signed in!</h1>
-                <Gallery />
+            <div className="signed-in-container">
+                <div className="signed-in">
+                    <h1>We're signed in!</h1>
+                    <Gallery />
+                </div>
             </div>
         );
     } else {
         page = (
-            <div className="signed-out">
-                <h1>This is a heading</h1>
-                <h3>Some random filler text</h3>
+            <div className="signed-out-container">
+                <div className="signed-out">
+                    <h1 className="inspiring-heading">Find your inspiration</h1>
+                    <h3 className="inspiring-text">Join the fotoshop community, home to several photos</h3>
+                    <h3 className="inspiring-text-two">and a handful of users</h3>
+                    <a href="/signup" className="start-for-free">Start for free</a>
+                </div>
             </div>
         )
     }
+
 
     return (
         <div className="home-body">
